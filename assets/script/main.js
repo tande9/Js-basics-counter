d035d46df67a3fb57e9844662b23484d6ad95bf9
tande9 <tandefadda@gmail.com>
2024-08-17 18:21:25 +0200

    fix some bugs

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,29 +1,37 @@
 // creazione elementi
 const mainContainer = document.querySelector('.counter-container');
 
-const buttonMinus =document.createElement('button');
-buttonMinus.textContent = '-';
-mainContainer.appendChild(buttonMinus);
-buttonMinus.classList.add('btn','btn-minus');
-
+//valore del counter
 let valueDisplayed = document.createElement('p');
-valueDisplayed.textContent = localStorage.getItem('value') || 0;
+valueDisplayed.textContent = parseInt(localStorage.getItem('value')) || 0;
 mainContainer.appendChild(valueDisplayed);
 valueDisplayed.classList.add('counter-value');
 
+//variabile per memorizzare il valore del DOM
+let currentValue = parseInt(valueDisplayed.textContent);
+
+//creazione bottoni
 const buttonPlus = document.createElement('button');
 buttonPlus.textContent = '+';
 mainContainer.appendChild(buttonPlus);
 buttonPlus.classList.add('btn','btn-plus');
 
+const buttonMinus =document.createElement('button');
+buttonMinus.textContent = '-';
+mainContainer.appendChild(buttonMinus);
+buttonMinus.classList.add('btn','btn-minus');
+
+//contenitore della challenge
 const challengeContainer = document.querySelector('.counter-header');
 
 let dynamicChallenge = document.createElement('h3');
 dynamicChallenge.textContent = `What number can you reach? First goal: 100`
 challengeContainer.appendChild(dynamicChallenge);
 
-// audio buttons
+//variabile per memorizzare il valore del DOM
+let challengeDisplayed = dynamicChallenge.textContent;
 
+// audio buttons
 const plusSound = new Audio();
 plusSound.src = "../assets/audio/plusbtn.mp3";
 
@@ -33,50 +41,54 @@ minusSound.src = "../assets/audio/minusbtn.mp3";
 const resetSound = new Audio();
 resetSound.src = "../assets/audio/reset-btn.mp3";
 
-// funzione del counter
+// audio settings
+function setAudio(audio){
+  audio.currentTime = 0;
+  audio.play();
+}
 
+//update valore
+function updateValue(newValue){
+currentValue = newValue;
+valueDisplayed.textContent = currentValue;
+
+//salvataggio valore
+localStorage.setItem('value',valueDisplayed.textContent);
+}
+
+// listener sui btns
+
+//PLUS
 buttonPlus.addEventListener('click',()=>{
-  let currentValue = parseInt(valueDisplayed.textContent);
-  currentValue ++;
-  valueDisplayed.textContent = currentValue;
+  updateValue(currentValue + 1)
   updateChallenge();
-  plusSound.currentTime = 0;
-  plusSound.play();
-
-  //salvataggio valore
-  localStorage.setItem('value',valueDisplayed.textContent);
+  setAudio(plusSound);
 })
 
+//MINUS
 buttonMinus.addEventListener('click',()=>{
-  let currentValue = parseInt(valueDisplayed.textContent);
-  currentValue --;
-  valueDisplayed.textContent = currentValue;
+  updateValue(currentValue - 1);
   updateChallenge();
-  minusSound.currentTime = 0;
-  minusSound.play();
-    //salvataggio valore
-    localStorage.setItem('value',valueDisplayed.textContent);
+  setAudio(minusSound);
 })
 
-//reset
-
+//reset value
 valueDisplayed.addEventListener('click', ()=>{
-  let currentValue = parseInt(valueDisplayed.textContent);
-  currentValue = 0;
-  valueDisplayed.textContent = currentValue;
-
-  resetSound.currentTime = 0;
-  resetSound.play();
+  updateValue(0);
+  setAudio(resetSound);
 })
 
 //altre funzionalità
 
+//update del text content dell'elemento challenge
 
+function updateValueOfTextChallenge (text){
+  challengeDisplayed = text;
+  dynamicChallenge.textContent = challengeDisplayed;
+}
 function updateChallenge(){
 
-  const currentValue = parseInt(valueDisplayed.textContent);
-
-  const MESSAGES = {
+  const messages = {
     goal100: "Nice, good job! Now we'll try to reach 500",
     goal500: "Congrats!! Nice work",
     almostThere: "Almost there! Keep it up",
@@ -87,27 +99,29 @@ function updateChallenge(){
   dynamicChallenge.classList.remove('challenge-wrong', 'challenge-right');
 
   if (currentValue >= 500) {
-    dynamicChallenge.textContent = MESSAGES.goal500;
+    updateValueOfTextChallenge(messages.goal500);
     dynamicChallenge.classList.add('challenge-right');
-  } else if (currentValue >= 400) {
-    dynamicChallenge.textContent = MESSAGES.almostThere;
-  } else if (currentValue >= 100) {
-    dynamicChallenge.textContent = MESSAGES.goal100;
-    dynamicChallenge.classList.add('challenge-right');
-  } else if (currentValue >= 70) {
-    dynamicChallenge.textContent = MESSAGES.almostThere;
-  } else if (currentValue <= -10) {
-    dynamicChallenge.textContent = MESSAGES.notCorrect;
-    dynamicChallenge.classList.add('challenge-wrong');
-  } else {
-    dynamicChallenge.textContent = MESSAGES.default;
-  }
 
+  } else if (currentValue >= 400 && currentValue <= 500) {
+    updateValueOfTextChallenge(messages.almostThere);
+
+  } else if (currentValue >= 100 && currentValue <= 115 ) {
+    updateValueOfTextChallenge(messages.goal100);
+    dynamicChallenge.classList.add('challenge-right');
+
+  } else if (currentValue >= 70 && currentValue <= 100) {
+    updateValueOfTextChallenge(messages.almostThere);
+
+  } else if (currentValue <= -10) {
+    updateValueOfTextChallenge(messages.notCorrect);
+    dynamicChallenge.classList.add('challenge-wrong');
+
+  } else {
+    updateValueOfTextChallenge(messages.default);
   }
+}
 
   //no zoom su mobile
   document.addEventListener('dblclick', function(e) {
     e.preventDefault();
-}, { passive: false });
-
-
+  }, { passive: false });
